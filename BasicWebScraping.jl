a757f04f3e276d5a10f5b703714c4ec8bcaabfce
DiracM <32461143+DiracM@users.noreply.github.com>
2023-08-22 15:55:50 +0100

    Update BasicWebScraping.jl

--- BasicWebScraping.jl
+++ BasicWebScraping.jl
@@ -5,6 +5,7 @@ using HTTP
  
 ## 1 - Get the website data
 # 1.1 - Get the http request
+ 
 site = "https://www.theguardian.com/world"
 r1 = HTTP.request("GET",site)
 http_str = String(r1.body)
@@ -15,14 +16,14 @@ write(f1,http_str)
  
 ## 2 - Search for the information
 # 2.1 - Find pointers
-search_str = "world/2022" #Keyword to search
+search_str = "russia"
 search_location = findall(search_str,http_str)
  
 #search_pnt = [search_location[i][1] for i=1:length(search_location)]
 search_pnt = hcat(search_location...)[1,:]
  
 #2.2 - Find the relevant String
-interval = 170
+interval = 150
 sub_strings_intervals = range.(search_pnt.-interval, search_pnt.+interval)
 str_array = String.(SubString.(http_str,sub_strings_intervals))
 arr1 = split.(str_array,"href=\"")
@@ -52,44 +53,3 @@ end
 # 3.2 - Save as an excel file
 using DelimitedFiles
 writedlm("data.csv", data_table, ',')
- 
-###########################################
-## 4 - SQLite interface
-###########################################
- 
-# 4.1 - Prepare the data for SQLite
-using SQLite; using DataFrames #import Pkg; Pkg.add("DataFrames")
- 
-# Clean data 
-data_table_string = permutedims(String.(hcat(data_table...)))
- 
-# 4.2 - Connect/create the database
-db = SQLite.DB("news.db")
-con = DBInterface
-# Create a table inside the DB
-SQLite.execute(db,"CREATE TABLE IF NOT EXISTS news_table(type TEXT,year TEXT,month TEXT,day TEXT,news TEXT)")
-# Check if the table was created
-SQLite.tables(db)
- 
-# 4.3 - Update the databe with new data
-for k = 1:size(data_table_string,1)
-    #Check if the entry exists already
-    df = DataFrame(con.execute(db, "SELECT * FROM news_table WHERE news = '$(data_table_string[k,end])'"))
-    if( size(df,1)<1)
-        #Add the data to the DB
-        sql_input = "INSERT INTO news_table(type,year,month,day,news) VALUES('$(data_table_string[k,1])','$(data_table_string[k,2])','$(data_table_string[k,3])','$(data_table_string[k,4])','$(data_table_string[k,5])')"
-        SQLite.execute(db,sql_input)
-    else
-        #It already exists
-    end
-end
- 
-# 4.4 - SQL commands
-df = DataFrame(con.execute(db, "SELECT * FROM news_table")) #Read the DB
- 
-data_table_string = [data_table_string; ["article" "2022" "dec" "01" "news"]] #Add a new line to DB
-df = con.execute(db, "DELETE FROM news_table WHERE news='news'") #Delete a selection of lines
-df = DataFrame(con.execute(db, "SELECT * FROM news_table WHERE news like '%pope%'")) #Search
- 
-df = con.execute(db, "DELETE FROM news_table") #Delete all, but leave the table
-df = con.execute(db, "DROP TABLE news_table") #Delete the table
